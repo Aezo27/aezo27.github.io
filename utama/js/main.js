f390fde2c43fd4eb6a39cb1f518dc8cafa85ed95
Aezo27 <stillhopeyou@gmail.com>
2020-05-29 10:26:02 +0700

    Minor Update

--- utama/js/main.js
+++ utama/js/main.js
@@ -133,7 +133,7 @@
     --------------------*/   
 
     // Use this for real timer date
-    var timerdate = "2020/05/29 10:00:00";
+    var timerdate = "2020/06/6 10:00:00";
 
 	$("#countdown").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"))
